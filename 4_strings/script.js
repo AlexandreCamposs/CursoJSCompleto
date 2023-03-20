5e88a4d57c062611801c251488f58baa042909aa
AleCampos <alemdcamposs@gmail.com>
2023-03-20 12:30:42 +0044

    operadores

---
+++ 4_strings/script.js
@@ -0,0 +1,4 @@
+console.log(typeof 'Oi, meu nome é Alexandre');
+console.log(typeof 'Eu queria comprar um carro.');
+console.log(typeof 'Infinity');
+console.log(typeof Infinity);
---
+++ 5_string_avancado/script.js
@@ -0,0 +1,5 @@
+console.log('Primeira linha \n Segunda linha');
+console.log("O meu nome pe 'Alexandre'");
+console.log('O nome dela é "Roberta"');
+console.log(`A multiplicação de 2 por 5 é: ${2 * 5}`);
+console.log("O meu " + "nome " + "é " + "Alexandre")
