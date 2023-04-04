2ad591781e16c4904cb6c31a0f5ddfbd7bd10811
AleCampos <alemdcamposs@gmail.com>
2023-04-04 21:21:51 +0044

    att

---
+++ nodeJs/6_job_finder/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+app.listen(PORT, function () {
+  console.log(`O express está rodando na porta ${PORT}`);
+});
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//db connection
+db.authenticate()
+  .then(() => {
+    console.log('Conectou ao banco com sucesso');
+  })
+  .catch((err) => ('Ocorreu um erro ao conectar', err));
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Está funcionando 3.');
+});
+
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
