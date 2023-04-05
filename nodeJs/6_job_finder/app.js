a2c86bbf26ac4fe041cb9040ddeb3df118f5cf37
AleCampos <alemdcamposs@gmail.com>
2023-04-05 21:34:43 +0044

    att

--- nodeJs/6_job_finder/app.js
+++ nodeJs/6_job_finder/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
+const Job = require('./models/Job');
 const PORT = 3000;
 
 app.listen(PORT, function () {
@@ -11,6 +14,14 @@ app.listen(PORT, function () {
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //db connection
 db.authenticate()
   .then(() => {
@@ -19,10 +30,19 @@ db.authenticate()
   .catch((err) => ('Ocorreu um erro ao conectar', err));
 
 //routes
-app.get('/', (req, res) => {
-  res.send('Está funcionando 3.');
-});
-
+// app.get('/',(req,res)=>{
+//   res.render('index')
+// })
+app.get('/',(req,res)=>{
+  Job.findAll({order:[
+    ['createdAt','DESC']
+  ]})
+  .then(jobs =>{
+    res.render('index',{
+      jobs
+    })
+  })
+})
 
 //jobs routes
 app.use('/jobs', require('./routes/jobs'));
--- nodeJs/6_job_finder/routes/jobs.js
+++ nodeJs/6_job_finder/routes/jobs.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
-router.get('/test',(req,res)=>{
-  res.send("Deu certo")
-})
+//rota de teste
+router.get('/test', (req, res) => {
+  res.send('Deu certo');
+});
 
+router.get('/add',(req,res)=>{
+  res.render('add')
+})
 
 //add job via post
 router.post('/add', (req, res) => {
@@ -24,4 +28,4 @@ router.post('/add', (req, res) => {
     .then(() => res.redirect('/'))
     .catch((err) => console.log(err));
 });
-module.exports = router ;
+module.exports = router;
